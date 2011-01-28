7e458180d11080ee982134a27cc20ca907032fb0
Michael Abrahamsen <michael.abrahamsen@gmail.com>
2011-01-28 09:14:48 -0700

    added Lexical Analyzer file and created enum values for tokens

---
+++ src/Compiler/LexicalAnalyzer/LexicalAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.LexicalAnalyzer
+{
+    public enum Tag
+    {
+        MP_AND = 256,
+        MP_BEGIN, MP_DIV, MP_DO, MP_DOWNTO,
+        MP_ELSE, MP_END, MP_FIXED, MP_FLOAT, MP_FOR,
+        MP_FUNCTION, MP_IF, MP_INTEGER, MP_MOD, MP_NOT,
+        MP_OR, MP_PROCEDURE, MP_PROGRAM, MP_READ, MP_REPEAT,
+        MP_THEN, MP_TO, MP_UNTIL, MP_VAR, MP_WHILE,
+        MP_WRITE
+    }
+    class LexicalAnalyzer
+    {
+
+    }
+}
---
+++ src/Compiler/LexicalAnalyzer/Token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.LexicalAnalyzer
+{
+    class Token
+    {
+        
+        public Token(int t)
+        {
+            Tag = t;
+        }
+
+        public int Tag
+        {
+            get;
+            set;
+        }
+
+        public string toString()
+        {
+            return Tag.ToString();
+        }
+    }
+}
