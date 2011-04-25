d93986a8fccf2f17f3bbd9367743267c182ef4d6
mikeabrahamsen <mail@michaelabrahamsen.com>
2011-04-25 11:23:08 -0600

    added VariableRecord class

---
+++ src/Compiler/SemAnalyzer/VariableRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compiler.Library;
+
+namespace Compiler.SemAnalyzer
+{
+    class VariableRecord
+    {
+        public IOMode ioMode
+        {
+            get;
+            set;
+        }
+        public VariableType variableType
+        {
+            get;
+            set;
+        }
+
+    }
+}
